3750fa186aad3563e53f0c628eaea4319a4ee3a2
Derek Li <derekli11204@gmail.com>
2022-02-25 01:16:09 -0500

    Add files via upload

--- Day10/hw5js.js
+++ Day10/hw5js.js
@@ -4,13 +4,22 @@
 // • Store a celsius temperature into a variable. Convert it to fahrenheit and output "NN°C is NN°F".
 // • Now store a fahrenheit temperature into a variable. Convert it to celsius and output "NN°F is
 // NN°C."
-let celsius1 = 10
 
-const convert = (celsius) => {
-    fahrenheit = celsius * 1.8 + 32
-    return `${celsius1}°C equals ${fahrenheit}°F`
+let celsius1 = 10;
+let fahrenheit1 = 80;
+
+const convertToFarenHeight = (celsius) => {
+    let fahrenheit = celsius * 1.8 + 32;
+    return `${celsius1}°C equals ${fahrenheit}°F`;
 }
-console.log(convert(celsius1))
+
+const convertToCelsius = (fahrenheit) => {
+    let celsius = (fahrenheit-32) * (5/9);
+    return `${fahrenheit1}°F equals ${celsius}°C`;
+}
+
+console.log(convertToFarenHeight(celsius1));
+console.log(convertToCelsius(fahrenheit1));
 
 // Use the BMI example from class activity (BMI question), and the code you already wrote, and improve it:
 // 1. Print a nice output to the console, saying who has the higher BMI. The message can be either "Lucas'
@@ -30,17 +39,20 @@ class person {
     }
 }
 
-const John = new person('John', 64, 130)
-const Steve = new person('Steve', 70,180)
+const John = new person('John', 64, 130);
+const Steve = new person('Steve', 70,180);
 
-JohnBMI = John.calculate(John.height, John.weight)
-SteveBMI = Steve.calculate(Steve.height, Steve.weight)
+JohnBMI = John.calculate(John.height, John.weight);
+SteveBMI = Steve.calculate(Steve.height, Steve.weight);
 
 if (JohnBMI > SteveBMI){
-    console.log(`John's BMI (${JohnBMI}) is greater than Steve's BMI (${SteveBMI})`)
+    console.log(`John's BMI (${JohnBMI}) is greater than Steve's BMI (${SteveBMI})`);
+}
+else if (SteveBMI > JohnBMI) {
+    console.log(`Steve's BMI (${SteveBMI}) is greater than John's BMI (${JohnBMI})`);
 }
 else {
-    console.log(`Steve's BMI (${SteveBMI}) is greater than John's BMI (${JohnBMI})`)
+    console.log(`John's BMI (${JohnBMI}) is the same as Steve's BMI (${SteveBMI})`);
 }
 
 // Use prompt() to let user type a number from the web page.
@@ -48,19 +60,19 @@ else {
 // If user enter 10, log "You win 10 point" to console;
 // If user typed 8, log "You win 8 points” to console
 // If the number is not 8 or 10, log "NOT MATCHED!!" to console
-let part3 = prompt('Please enter a number!')
+let part3 = prompt("Please enter a number!")
 
 if ( part3 == "" || isNaN(parseInt(part3))) {
-    console.log('Please enter a number!')
+    console.log('Please enter a number!');
 }
 else if (parseInt(part3) === 10) {
-    console.log('You win 10 points')
+    console.log('You win 10 points');
 }
 else if (parseInt(part3) === 8) {
-    console.log('You win 8 points')
+    console.log('You win 8 points');
 }
 else {
-    console.log('NOT MATCHED!!')
+    console.log('NOT MATCHED!!');
 }
 
 // Create a function to check who the winner is:
@@ -72,41 +84,41 @@ else {
 // Data 1: Nets score 80, 82 and 100, Knicks score 80, 90 and 106
 // Data 2: Nets score 98, 110 and 101. Knicks score 108, 92 and 110
 
-const Nets1 = [80,82,100]
-const Knicks1 = [80,90,106]
-const Nets2 = [98,110,101]
-const Knicks2 = [108,92,110]
+const Nets1 = [80,82,100];
+const Knicks1 = [80,90,106];
+const Nets2 = [98,110,101];
+const Knicks2 = [108,92,110];
 
 const average = (team) => {
-    sum = 0
+    sum = 0;
     for (let i=0;i<team.length;i++){
-        sum += team[i]
+        sum += team[i];
     }
-    return (sum/team.length)
+    return (sum/team.length);
 }
-nMean = average(Nets1)
-kMean = average(Knicks1)
+nMean = average(Nets1);
+kMean = average(Knicks1);
 
-nMean2 = average(Nets2)
-kMean2 = average(Knicks2)
+nMean2 = average(Nets2);
+kMean2 = average(Knicks2);
 
 const winner = (mean1, mean2) => {
     if (mean1 > mean2 && mean1 >= 100){
-        console.log('Nets Win')
+        console.log('Nets Win');
     }
     else if (mean2 > mean1 && mean2 >= 100){
-        console.log('Knicks Win')
+        console.log('Knicks Win');
     }
     else if (mean1 === mean2 && mean1 === 100){
-        console.log('Tie')
+        console.log('Tie');
     }
     else {
-        console.log('No winners')
+        console.log('No winners');
     }
 }
 
-winner(nMean, kMean)
-winner(nMean2, kMean2)
+winner(nMean, kMean);
+winner(nMean2, kMean2);
 
 
 
